f05725c5c355faaaf4b9368e93c7cbc4295f69d1
navarrofeliza <navarrofeliza@yahoo.com>
2022-02-13 18:59:50 -0800

    first commit w/ errors

---
+++ DojoSurveyValidation/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurveyValidation.Models;
+
+namespace DojoSurveyValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult HandleFormSubmission(Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", survey);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
